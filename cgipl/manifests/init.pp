66f4da8eb1cd22a4e3f45916699c0eb84f36a9e9
mj1492mjmj <mj1492@seznam.cz>
2013-06-10 09:16:36 +0200

    Create init.pp

---
+++ cgipl/manifests/init.pp
@@ -0,0 +1,45 @@
+class cgipl { 
+
+notify {"zacina se instalovat":}
+
+  package { 'cgipl':
+		name   => 'httpd',
+		ensure => installed,
+
+	}
+
+	service  { 'cgipl':
+        name   	=> 'httpd',
+	ensure	=> running,
+	enable 	=> true,
+	subscribe => File['httpd.conf'],
+	}
+
+
+
+	file { "/var/www/cgi-bin/cgi.pl":
+		owner  => root,
+		group  => root,
+		mode   => 755,
+		source => "puppet:///modules/cgipl/cgi.pl",
+		}
+
+        file { 'httpd.conf':
+                path   => '/etc/httpd/conf/httpd.conf',
+		owner  => root,
+                group  => root,
+                mode   => 644,
+                source => "puppet:///modules/cgipl/httpd.conf",
+                }
+# jde to i jinak. Jde to i do podadresare
+# demonstracni soubor nulove delky test.txt je v: /etc/puppetlabs/puppet/modules/cgipl/files/test
+
+
+       file { "/tmp/cgi.plxxxxxx":
+                owner  => root,
+                group  => root,
+                mode   => 644,
+                source => "puppet:///modules/cgipl/test/test.txt",
+                }
+
+}
