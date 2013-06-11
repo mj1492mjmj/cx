c4297a80abcaebc096614a1ae9f54c1292e25298
mj1492mjmj <mj1492@seznam.cz>
2013-06-11 07:49:45 +0200

    Update cgi.pl

--- cgipl/files/cgi.pl
+++ cgipl/files/cgi.pl
@@ -14,10 +14,15 @@ print "Content-Type: text/html\n\n";
 
 my $srv = `hostname`;
 my $upt = `uptime`;
-my $res = `who -r`;
+my $res = `/usr/bin/vmstat | tail -10`;
 my $mnt = `mount`;
 my $rlv = `cat /etc/inittab | grep id | tail -1 | cut -c 4`;
 my $ifaddr = `ifconfig`;
+my $tst = `/bin/cat /etc/hosts`;
+my $rslv = `/bin/cat /etc/resolv.conf`;
+my $rlv2 = `/bin/uname -a`;
+
+
 
 print "<html> <head>\n";
 print "<title>Server: $srv</title>";
@@ -33,7 +38,11 @@ print "Uptime:  ", "<BR>", $upt , "<BR>", "\n";
 print "Svazky:  ", "<BR>", $mnt , "<BR>", "\n";
 print "Run Level:  ", $rlv , "<BR>", "\n";
 print "Sítě:  ", "<BR>", $ifaddr , "<BR>", "\n";
+print "etc:  ", "<BR>", $tst , "<BR>", "\n";
+print "resolv.conf:  ", "<BR>", $rslv , "<BR>", "\n";
+print "Memory:  ", "<BR>", $res , "<BR>", "\n";
+print "Uname:  ", "<BR>", $rlv2 , "<BR>", "\n";
+;
 print "<HR></PRE>", "\n";
 print "<p></p>\n";
 print "</body> </html>\n"
-
